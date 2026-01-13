c639cb66ab82a1055c3e7a120a71bdc2547df4c3
by1337 <by133700@gmail.com>
2026-01-13 09:08:58 +0300

    feat: scoreboard command

--- src/main/java/org/by1337/bparser/commands/ScoreboardCopyCommand.java
+++ src/main/java/org/by1337/bparser/commands/ScoreboardCopyCommand.java
@@ -3,85 +3,76 @@ package org.by1337.bparser.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-
-import org.by1337.bparser.BParser;
-import org.by1337.bparser.text.RawMessageConvertor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.scores.*;
+import org.by1337.bparser.text.ComponentUtil;
 import org.by1337.bparser.util.ChatUtil;
 
-import java.util.Collection;
 import java.util.Comparator;
 
 public class ScoreboardCopyCommand {
-   // private static final Comparator<ScoreboardEntry> VANILLA_ORDER = Comparator.comparing(ScoreboardEntry::value).reversed().thenComparing(ScoreboardEntry::owner, String.CASE_INSENSITIVE_ORDER);
+    private static final Comparator<PlayerScoreEntry> VANILLA_ORDER = Comparator.comparing(PlayerScoreEntry::value).reversed().thenComparing(PlayerScoreEntry::owner, String.CASE_INSENSITIVE_ORDER);
 
 
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(LiteralArgumentBuilder.<FabricClientCommandSource>literal("//scoreboard")
                 .executes(ctx -> {
+                    Minecraft mc = Minecraft.getInstance();
+                    Scoreboard scoreboard = mc.level.getScoreboard();
+                    Objective objective = scoreboard.getDisplayObjective(DisplaySlot.SIDEBAR);
+
+                    if (objective != null) {
+                        Component header = objective.getDisplayName();
+                        MutableComponent headerText = Component.literal("Header: ").append(header);
+                        ChatUtil.addCopyButton(headerText, ComponentUtil.convert(header));
+                        ChatUtil.show(headerText);
+
+                        var scores = scoreboard.listPlayerScores(objective);
+                        MutableComponent all = Component.literal("");
+                        all.append(header).append("\n");
+                        scores
+                                .stream()
+                                .sorted(VANILLA_ORDER)
+                                .forEach(score -> {
+                                    String playerName = score.owner();
+                                    PlayerTeam team = scoreboard.getPlayersTeam(playerName);
+                                    Component lineText;
+                                    if (team != null) {
+                                        String cleaned = playerName.replaceAll("§[0-9A-FK-ORa-fk-or]", "");
+                                        if (cleaned.isEmpty()) {
+                                            lineText = team.getFormattedName(Component.literal(""));
+                                        } else {
+                                            lineText = team.getFormattedName(Component.literal(playerName));
+                                        }
+                                    } else {
+                                        lineText = Component.literal(playerName);
+                                    }
+                                    String scoreCount;
+                                    if (score.value() < 10) {
+                                        scoreCount = "0" + score.value() + " ";
+                                    } else {
+                                        scoreCount = score.value() + " ";
+                                    }
+                                    ChatUtil.show(
+                                            ChatUtil.addCopyButton(
+                                                    Component.literal(scoreCount).append(lineText),
+                                                    ComponentUtil.convert(lineText)
+                                            )
+                                    );
+                                    all.append(lineText).append("\n");
+                                });
+                        ChatUtil.show(
+                                ChatUtil.addCopyButton(
+                                        Component.literal(""),
+                                        "[copy all]",
+                                        ComponentUtil.convert(all)
+                                )
+                        );
+                    }
                     return 1;
                 })
         );
     }
 }
-//{
-//                    MinecraftClient client = MinecraftClient.getInstance();
-//                    Scoreboard scoreboard = client.world.getScoreboard();
-//
-//                    ScoreboardObjective objective = scoreboard.getObjectiveForSlot(ScoreboardDisplaySlot.SIDEBAR);
-//
-//                    if (objective != null) {
-//                        Text header = objective.getDisplayName();
-//                        MutableText headerText = Text.literal("Header: ").append(header);
-//                        ChatUtil.addCopyButton(headerText, RawMessageConvertor.convert(Text.Serialization.toJsonString(header)));
-//                        ChatUtil.show(headerText);
-//
-//                        Collection<ScoreboardEntry> scores = scoreboard.getScoreboardEntries(objective);
-//                        MutableText all = Text.literal("");
-//                        all.append(header).append("\n");
-//                        scores
-//                                .stream()
-//                                .sorted(VANILLA_ORDER)
-//                                .forEach(score -> {
-//                                    BParser.LOGGER.info(score);
-//                                    String playerName = score.owner();
-//                                    Team team = scoreboard.getScoreHolderTeam(playerName);
-//
-//                                    Text lineText;
-//                                    if (team != null) {
-//                                        String cleaned = playerName.replaceAll("§[0-9A-FK-ORa-fk-or]", "");
-//                                        if (cleaned.isEmpty()) {
-//                                            lineText = team.decorateName(Text.literal(""));
-//                                        } else {
-//                                            lineText = team.decorateName(Text.literal(playerName));
-//                                        }
-//                                    } else {
-//                                        lineText = Text.literal(playerName);
-//                                    }
-//
-//                                    String scoreCount;
-//                                    if (score.value() < 10) {
-//                                        scoreCount = "0" + score.value() + " ";
-//                                    } else {
-//                                        scoreCount = score.value() + " ";
-//                                    }
-//
-//                                    ChatUtil.show(
-//                                            ChatUtil.addCopyButton(
-//                                                    Text.literal(scoreCount).append(lineText),
-//                                                    RawMessageConvertor.convert(Text.Serialization.toJsonString(lineText))
-//                                            )
-//                                    );
-//                                    all.append(lineText).append("\n");
-//                                });
-//                        ChatUtil.show(
-//                                ChatUtil.addCopyButton(
-//                                        Text.literal(""),
-//                                        "[copy all]",
-//                                        RawMessageConvertor.convert(Text.Serialization.toJsonString(all)).replace("<br>", "\n")
-//                                )
-//                        );
-//                    }
-//
-//
-//                    return 1;
-//                }
